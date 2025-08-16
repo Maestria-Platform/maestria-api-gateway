ca45cfc2f1655563d6c0edc9b58761301027b8ad
Matheus Maciel <srmatheusmaciel@hotmail.com>
2025-08-16 13:30:01 -0300

    initial commit

---
+++ src/main/java/br/com/maestria/maestria_api_gateway/MaestriaApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.maestria.maestria_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaestriaApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaestriaApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/maestria/maestria_api_gateway/MaestriaApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maestria.maestria_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaestriaApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
